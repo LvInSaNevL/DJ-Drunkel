751f05df5abd1217944513707613a131afcbcc60
Matt <33157232+LvInSaNevL@users.noreply.github.com>
2024-09-25 09:29:08 -0400

    Token regen script
    Added a helper script, `regen_token.py` to help automate regenerating the token when needed

--- youtube.py
+++ youtube.py
@@ -8,6 +8,7 @@ import re
 from urllib.parse import urlparse
 import os
 import requests
+import json
 
 import google.auth.exceptions
 import google_auth_oauthlib
@@ -20,25 +21,11 @@ import googleapiclient.errors
 # Variables and such
 scopes = ["https://www.googleapis.com/auth/youtube.force-ssl",
           "https://www.googleapis.com/auth/youtube.readonly"]
-playlist = "PLRDuNIkwpnsdsAaytTIOtkaQGcsdy_fJH"
+playlist = "PLRDuNIkwpnsfJaOX5Bq3jrPtP2WfP_vBl"
 lastAuth = datetime.datetime.min       
 
-def refresh_token(client_id, client_secret, refresh_token):
-    token_url = "https://accounts.spotify.com/api/token"
-    data = {
-        "grant_type": "refresh_token",
-        "refresh_token": refresh_token,
-        "client_id": client_id,
-        "client_secret": client_secret
-    }
-
-    response = requests.post(token_url, data=data)
-    token_info = response.json()
-    return token_info.get("access_token")
-
-# Authenticates to the Google API
 def get_authenticated_service(lastAuth):
-    utils.logPrint("Authenticating YouTube service access and refresh token", 0)
+    utils.logPrint("Authenticating service access and refresh token", 0)
     nowAuth = datetime.datetime.now()
     authDiff = nowAuth - lastAuth
     if (authDiff.total_seconds() * 1000 < 250):
@@ -49,29 +36,37 @@ def get_authenticated_service(lastAuth):
     api_service_name = "youtube"
     api_version = "v3"
     client_secrets_file = "youtubeauth.json"
-    redirect_uri = "http://localhost:5656"
-    cred_token = None
 
-    # Try to use the refresh token first
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
+
     if os.path.exists("refresh.token"):
-        try:
-            cred_token = Credentials.from_authorized_user_file('refresh.token')
-            cred_token.refresh(Request())
-        except google.auth.exceptions.RefreshError as e:
-            print(e)
-    # Manually authenticate with the user if no token is available  
-    else:
-        credFlow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
-        cred = credFlow.run_local_server(
-            host='localhost',
-            port=5656,
-            authorization_prompt_message='Please visit this URL: {url}',
-            success_message='The auth flow is complete; you may close this window.',
-            open_browser=False
-        )
-        with open('refresh.token', 'w') as token:
-            token.write(cred.to_json())
-    return googleapiclient.discovery.build(api_service_name, api_version, credentials=cred_token)
+        with open("refresh.token", 'r') as t:
+            rawToken = t.read()
+            with open(client_secrets_file, 'r') as j:
+                youtubeAuth = json.loads(j.read())
+            params = {
+                "grant_type": "refresh_token",
+                "client_id": youtubeAuth['installed']['client_id'],
+                "client_secret": youtubeAuth['installed']['client_secret'],
+                "refresh_token": rawToken
+            }
+            authorization_url = "https://www.googleapis.com/oauth2/v4/token"
+            r = requests.post(authorization_url, data=params)
+            credToken = google.oauth2.credentials.Credentials(
+                                                    rawToken,
+                                                    refresh_token = rawToken,
+                                                    token_uri = 'https://accounts.google.com/o/oauth2/token',
+                                                    client_id = youtubeAuth['installed']['client_id'],
+                                                    client_secret = youtubeAuth['installed']['client_secret']
+                                                    )
+    else: 
+        credFlow = flow.run_console()
+        with open('refresh.token', 'w+') as f:
+            credToken = credFlow
+            f.write(credFlow._refresh_token)
+            utils.logPrint('Saved Refresh Token to file: refresh.token', 0)
+    
+    return googleapiclient.discovery.build(api_service_name, api_version, credentials=credToken)
 
 ### <summary>
 # Adds a new video to the playlist
